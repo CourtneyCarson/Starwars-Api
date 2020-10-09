42383f532bcad629ee64f531beba7c0fab1c9e87
Courtney Carson <cmacarson@gmail.com>
2020-10-09 09:19:31 -0700

    wireframe header, searchbox, app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Header from './Header'
+import SearchBox from './SearchBox'
 
-function App() {
-  return (
+
+class App extends React.Component {
+  render(){
+  return  (
     <main className='App'>
+      <Header />
+      <SearchBox/> 
+  
       {/* content goes here */}
     </main>
   );
 }
-
+}
 export default App;
---
+++ src/SearchForm.js
@@ -0,0 +1,9 @@
+// import React form 'react'
+// import SearchBox from './SearchBox'
+
+
+// <section className="search-box">
+//       <label>Search</label>
+//       <input type="text" onChange={e => props.onQueryChange(e.target.value)}/>
+//       <button type="submit">Search</button>
+//     </section>
